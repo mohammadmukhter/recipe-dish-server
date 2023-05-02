6af9ec5ce1049386326fed00c0f5f00c08a97c06
MUKHTER <mohammadmukhter@gmail.com>
2023-05-03 01:11:09 +0600

    added recipe search by id

--- index.js
+++ index.js
@@ -15,6 +15,13 @@ app.get("/chef", (req,res)=>{
     res.send(chefData)
 })
 
+app.get("/recipe/:id", (req,res)=> {
+    const recipeId = req.params.id;
+   
+    const findData = chefData.chefs.find((data) => data.id === recipeId);
+    res.send(findData)
+})
+
 app.listen(port, ()=>{
     console.log('connected successfully')
 })
